18595e632d0c18365a602748256ca8cd428da72b
Raphael kiteme <raphael.kituku@gmail.com>
2023-09-13 13:30:36 +0400

    created a script.js file to store horoscope readings in an object.

---
+++ script.js
@@ -0,0 +1,8 @@
+//Horoscope readings for each sign
+const horoscopeReadings = {
+  aries: "Today is a great day for new beginnings. Take charge of your goals and make things happen.",
+  taurus: "You may face some challenges today, but stay focused and determined. Your efforts will pay off.",
+ 
+  
+  // Add more horoscope readings for other signs
+};
