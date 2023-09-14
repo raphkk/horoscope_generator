fedc0cbd5e86636fe2b4e96891c8a026dbf2e36a
Raphael kiteme <raphael.kituku@gmail.com>
2023-09-14 21:38:15 +0400

    added more information in my object for horoscopes

--- script.js
+++ script.js
@@ -2,7 +2,9 @@
 const horoscopeReadings = {
   aries: "Today is a great day for new beginnings. Take charge of your goals and make things happen.",
   taurus: "You may face some challenges today, but stay focused and determined. Your efforts will pay off.",
- 
+  gemini: "Communication is key today. Express yourself clearly and listen to others to avoid misunderstandings.",
+  cancer: "Focus on self-care today. Take some time for relaxation and reflection to rejuvenate your mind and body.",
+  leo: "Some friends associated with a group you admire might take you to some kind of rally, Leo. This could be a lot of fun for you. You could make some new friends. You're naturally inclined toward humanitarian concerns, and these might be the focus of some animated discussions today. Take in all the information and consider it later. Right now your mind is overwhelmed!",
   
   // Add more horoscope readings for other signs
 };
